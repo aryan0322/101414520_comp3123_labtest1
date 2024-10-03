b1ef4e98f0308035a6261e17b92e826c8547e32f
aryan0322 <156135876+aryan0322@users.noreply.github.com>
2024-10-03 19:01:27 -0400

    question-1 updated

--- question-1.js
+++ question-1.js
@@ -2,16 +2,23 @@
 const mixedArray = ['PIZZA',10,true,25,false,'Wings']
 
 // creating function named lowerCaseWords
-lowerCaseWords = (arr) => {
+lowerCaseWords = (array) => {
     return new Promise(
-        (resolve, reject) => {
-            const resultArray = arr
-                .filter(item => typeof item === 'string')
-                .map(item => item.toLowerCase())
-            resolve(resultArray)
+            (resolve, reject) => {
+                if(!Array.isArray(array)){
+                    reject(new Error("Input in not an array"));
+                }
+                else{
+                const resultArray = array
+                    .filter(item => typeof item === 'string')
+                    .map(item => item.toLowerCase())
+                resolve(resultArray)
+            }
         }
     )
 }
 
 // checking output
-lowerCaseWords(mixedArray).then(result => console.log(result))
+lowerCaseWords(mixedArray)
+    .then(result => console.log(result))
+    .catch(error => console.error(error))
